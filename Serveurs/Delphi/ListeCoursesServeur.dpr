30d63022cb2a6b9f4316d91c3565a13e23018f71
patrick@premartin.fr <Patrick>
2022-01-24 18:32:14 +0100

    ajout aide (-h), ajout lancement démon Linux (-daemon), test console Windows 32/64 et Linux

--- Serveurs/Delphi/ListeCoursesServeur.dpr
+++ Serveurs/Delphi/ListeCoursesServeur.dpr
@@ -2,6 +2,10 @@ program ListeCoursesServeur;
 {$APPTYPE CONSOLE}
 
 uses
+{$IFDEF LINUX}
+  Posix.Stdlib, Posix.SysStat, Posix.SysTypes, Posix.Unistd, Posix.Signal,
+  Posix.Fcntl,
+{$ENDIF}
   System.SysUtils,
   System.Types,
   IPPeerServer,
@@ -9,7 +13,7 @@ uses
   IdHTTPWebBrokerBridge,
   Web.WebReq,
   Web.WebBroker,
-  WebModuleUnit1 in 'WebModuleUnit1.pas' {WebModule1: TWebModule},
+  WebModuleUnit1 in 'WebModuleUnit1.pas' {WebModule1: TWebModule} ,
   ServerConst1 in 'ServerConst1.pas',
   uDM in 'uDM.pas' {dm: TDataModule};
 
@@ -144,22 +148,145 @@ begin
   end;
 end;
 
+{$IFDEF LINUX}
+
+const
+  // Missing from linux/StdlibTypes.inc !!! <stdlib.h>
+  EXIT_FAILURE = 1;
+  EXIT_SUCCESS = 0;
+
+var
+  pid: pid_t;
+  fid: Integer;
+  idx: Integer;
+  running: Boolean;
+
+procedure HandleSignals(SigNum: Integer); cdecl;
+begin
+  case SigNum of
+    SIGTERM:
+      begin
+        running := False;
+      end;
+    SIGHUP:
+      begin
+        // syslog(LOG_NOTICE, 'daemon: reloading config');
+        // Reload configuration
+      end;
+  end;
+end;
+
+procedure RunServerDaemon(APort: Integer);
+var
+  LServer: TIdHTTPWebBrokerBridge;
+begin
+  LServer := TIdHTTPWebBrokerBridge.Create(nil);
+  try
+    LServer.DefaultPort := APort;
+    StartServer(LServer);
+    while running do
+      sleep(100);
+  finally
+    LServer.Free;
+  end;
+end;
+
+{$ENDIF}
+
 var
   port: Integer;
   paramvalue: string;
 
 begin
   try
-    if FindCmdLineSwitch('port', paramvalue, True, [clstvaluenextparam]) then
-      port := paramvalue.ToInteger
+    if findcmdlineswitch('h') then
+    begin
+      Writeln('ListeCoursesServeur');
+      Writeln('(c) 2022 Patrick Prémartin');
+      Writeln('');
+      Writeln('-h => display this help');
+      Writeln('-port number => port number to listen (8080 by default)');
+{$IFDEF LINUX}
+      Writeln('-daemon => start the server as Linux daemon');
+{$ENDIF}
+    end
     else
-      port := 8080;
+    begin
+      if findcmdlineswitch('port', paramvalue, True, [clstvaluenextparam]) then
+        port := paramvalue.ToInteger
+      else
+        port := 8080;
 
-    if WebRequestHandler <> nil then
-      WebRequestHandler.WebModuleClass := WebModuleClass;
-    RunServer(port);
+      if WebRequestHandler <> nil then
+        WebRequestHandler.WebModuleClass := WebModuleClass;
+
+{$IFDEF LINUX}
+      // Want to understand how to create a Linux daemon ?
+      // Look at Paolo Rossi blog :
+      // https://blog.paolorossi.net/building-a-real-linux-daemon-with-delphi-part-1/
+      // https://blog.paolorossi.net/building-a-real-linux-daemon-with-delphi-part-2/
+      if findcmdlineswitch('daemon') then
+      begin
+        // openlog(nil, LOG_PID or LOG_NDELAY, LOG_DAEMON);
+        try
+          if getppid() > 1 then
+          begin
+            pid := fork();
+            if pid < 0 then
+              raise exception.Create('Error forking the process');
+
+            if pid > 0 then
+              Halt(EXIT_SUCCESS);
+
+            if setsid() < 0 then
+              raise exception.Create
+                ('Impossible to create an independent session');
+
+            Signal(SIGCHLD, TSignalHandler(SIG_IGN));
+            Signal(SIGHUP, HandleSignals);
+            Signal(SIGTERM, HandleSignals);
+
+            pid := fork();
+            if pid < 0 then
+              raise exception.Create('Error forking the process');
+
+            if pid > 0 then
+              Halt(EXIT_SUCCESS);
+
+            for idx := sysconf(_SC_OPEN_MAX) downto 0 do
+              __close(idx);
+
+            fid := __open('/dev/null', O_RDWR);
+            dup(fid);
+            dup(fid);
+
+//            umask(027);
+
+//            chdir('/');
+          end;
+
+          running := True;
+
+          RunServerDaemon(port);
+
+          ExitCode := EXIT_SUCCESS;
+        except
+          on E: exception do
+          begin
+            // syslog(LOG_ERR, 'Error: ' + E.Message);
+            ExitCode := EXIT_FAILURE;
+          end;
+        end;
+
+        // syslog(LOG_NOTICE, 'daemon stopped');
+        // closelog();
+      end
+      else
+{$ENDIF}
+        RunServer(port);
+    end;
   except
-    on E: Exception do
+    on E: exception do
       Writeln(E.ClassName, ': ', E.Message);
   end
 
--- Serveurs/Delphi/TODO.md
+++ Serveurs/Delphi/TODO.md
@@ -1,4 +1,2 @@
 # TODO List du serveur WebBroker développé sous Delphi
 
-* adapter le programme pour fonctinner en démon Linux
-* documenter le programme (paramètre -H)
